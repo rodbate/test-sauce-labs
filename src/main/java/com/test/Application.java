b96b5a9c3b7bd7a24ee4c451cd38669317c18816
rodbate <ssjiang@incardata.com.cn>
2015-10-22 17:39:24 +0800

    1111

---
+++ src/main/java/com/test/Application.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by rodbate on 2015/10/22.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
